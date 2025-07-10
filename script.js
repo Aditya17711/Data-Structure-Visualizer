ae074c4fa5f2f1570882f73c8d70d3e2bdf702ec
Aditya17711 <Adityakumar17711@gmail.com>
2025-07-10 14:16:31 +0530

    Delete script.js
    Deleting script.js uploaded by mistake

--- script.js
+++
@@ -1,6 +0,0 @@
-
-function show_Stack() {
-    document.querySelector(".home_page_container").style.display = "none";
-    document.querySelector(".stack_main_container").style.display = "block";
-    document.querySelector(".queue_main_container").style.display = "none";
-}
